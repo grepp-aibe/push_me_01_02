ef9de87b13a126972049026bbca2dbdc0cd45997
qus0in <qus0in@gmail.com>
2025-01-27 03:58:36 +0900

    fix

--- problem05/problem05.test.js
+++ problem05/problem05.test.js
@@ -2,7 +2,7 @@
 const sumOfSmallProducts = require("./problem05");
 
 describe("problem05: sumOfSmallProducts", () => {
-  test("1부터 2까지 => (1*1 + 1*2 + 2*1 + 2*2) => 10", () => {
+  test("1부터 2까지 => (1*1 + 1*2 + 2*1 + 2*2) => 9", () => {
     expect(sumOfSmallProducts(2)).toBe(10);
   });
 
@@ -10,8 +10,8 @@ describe("problem05: sumOfSmallProducts", () => {
     expect(sumOfSmallProducts(3)).toBe(36);
   });
 
-  test("1부터 4까지 => (모든 i*j <= 10인 경우 합산) => 100", () => {
-    expect(sumOfSmallProducts(4)).toBe(100);
+  test("1부터 4까지 => (모든 i*j <= 10인 경우 합산) => 60", () => {
+    expect(sumOfSmallProducts(4)).toBe(60);
   });
 
   test("1부터 1까지 => (1*1) => 1", () => {
