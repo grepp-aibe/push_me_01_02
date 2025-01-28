7df703c81f593baad2d759b0f5afe920286e2f4a
soowan <nick6767676767@gmail.com>
2025-01-29 02:22:25 +0900

    fix

--- problem01/problem01.js
+++ problem01/problem01.js
@@ -10,13 +10,13 @@ function grade(score) {
   let result;
 
   if (score > 100 || score < 0) {
-    result = "C"; // 🚨
+    result = "Invalid"; // 🚨
   } else if (score >= 90) {
-    result = "B"; // 🚨
+    result = "A"; // 🚨n
   } else if (score >= 80) {
-    result = "A"; // 🚨
-  } else {
     result = "B"; // 🚨
+  } else {
+    result = "C"; // 🚨
   }
 
   return result;
